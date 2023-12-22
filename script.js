1321f5a6d71225578f6ff079031bf8baf4df6370
nexola <vitormatheusfv@gmail.com>
2023-12-22 11:37:22 -0300

    Prototypes

--- script.js
+++ script.js
@@ -7,16 +7,40 @@ class Pessoa {
     this.sobrenome = sobrenome;
     this.anoNasc = anoNasc;
   }
-
-  printNome() {
-    console.log(this.primeiroNome + " " + this.sobrenome);
-  }
 }
 
 const vitor = new Pessoa("Vitor", "Vianna", 1999);
 
 console.log(vitor);
+console.log(vitor instanceof Pessoa);
+
+// Prototypes
+Pessoa.prototype.printNome = function () {
+  console.log(this.primeiroNome + " " + this.sobrenome);
+};
+
 vitor.printNome();
 
-console.log(vitor instanceof Pessoa);
-console.log(vitor.country);
+// .prototypeOfLinkedObjects
+
+Pessoa.prototype.especie = "Homo Sapiens";
+
+console.log(vitor);
+console.log(vitor.hasOwnProperty("primeiroNome"));
+console.log(vitor.hasOwnProperty("especie"));
+
+console.log(vitor.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+const arr = [3, 6, 6, 4, 5, 9, 4, 5];
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+
+console.log(h1.__proto__);
+
+console.dir((x) => x + 1);
